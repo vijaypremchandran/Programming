314af10a0acf413f2bf782c40c9c033151a5d8fe
Vijay Chandran <vijayprem.chandran@gmail.com>
2021-03-27 14:10:10 -0400

    adding index file for the callback project

---
+++ Javascript/callback/app.js
@@ -0,0 +1,16 @@
+// Define post
+
+const post = [
+    {title  :'Post one', body   :'This is post one'}
+    {title  :'Post two', body   :'This is post two'}
+]
+
+//Define 2 function. create post and get post.
+
+function createPost(){
+
+};
+
+function getPost(){
+
+};
