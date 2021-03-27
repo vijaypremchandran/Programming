cb33e2528451e31a74f58b50913e5db0d9791868
Vijay Chandran <vijayprem.chandran@gmail.com>
2021-03-27 14:38:06 -0400

    add changes to call back project

--- Javascript/callback/app.js
+++ Javascript/callback/app.js
@@ -1,16 +1,56 @@
 // Define post
 
-const post = [
-    {title  :'Post one', body   :'This is post one'}
+const posts = [
+    {title  :'Post one', body   :'This is post one'},
     {title  :'Post two', body   :'This is post two'}
 ]
 
+// //Define 2 function. create post and get post.
+
+// function createPost(){
+//     setTimeout(function(){
+//         posts.push(post);
+//     }, 2000);
+// };
+
+// function getPost(){
+//     setTimeout(function(){
+//         let output = '';
+//         posts.forEach(function(post) {
+//             output += `<li>${post.title}</li>`
+//         })
+//         document.body.innerHTML = output;
+//     }, 1000);
+// };
+
+// // call the posts function to create posts.
+
+// createPost({title  :'Post three', body   :'This is post three'})
+
+//get post function
+
+//getPost();
+
+
 //Define 2 function. create post and get post.
 
-function createPost(){
-
+function createPost(post, callback){
+    setTimeout(function(){
+        posts.push(post);
+        callback();
+    }, 2000);
 };
 
-function getPost(){
-
+function getPosts(){
+    setTimeout(function(){
+        let output = '';
+        posts.forEach(function(post) {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output;
+    }, 1000);
 };
+
+// call the posts function to create posts.
+
+createPost({title  :'Post three', body   :'This is post three'}, getPosts);
