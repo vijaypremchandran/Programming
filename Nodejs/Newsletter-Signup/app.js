92d286cae2a215b273b73da5b5cb458704bb6a1e
Vijay Chandran <vijayprem.chandran@gmail.com>
2021-03-31 23:06:42 -0400

    finishing newletter project

--- Nodejs/Newsletter-Signup/app.js
+++ Nodejs/Newsletter-Signup/app.js
@@ -4,10 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+const mailchimp = require("@mailchimp/mailchimp_marketing")
 
 //use the static files like images and style sheets.
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//use the mailchimp credentials 
+mailchimp.setConfig({ 
+    apiKey: "0bec30a1902d50532081aa2fe41ac587-us1",
+    server: "us1"
+});
+
+const listId = "1adc9e2656";
 
 
 // get the initial page and respond
@@ -24,15 +33,40 @@ app.post("/", function(req,res){
     var lastName = req.body.lName;
     var email = req.body.email;
 
-    console.log(firstName +' '+ lastName +' '+ email)
-    res.send("Data posted to the server");
+    const subscribingUser = {
+        firstName: req.body.fName,
+        lastName: req.body.lName,
+        email: req.body.email
+      };
 
+      async function run() {
+          try {
+            await mailchimp.lists.addListMember(listId, {
+                email_address: subscribingUser.email,
+                status: "subscribed",
+                merge_fields: {
+                  FNAME: subscribingUser.firstName,
+                  LNAME: subscribingUser.lastName
+                }
+              });
+            res.sendFile(__dirname + "/sucess.html");
+          }
+          catch (e) {
+              console.log(`error ${e.message}`)
+              res.sendFile(__dirname + "/failure.html");
+          }
+        
+      }
+      
+      run();
 });
+
+//post from the failure page route.
+app.post("/failure", function(req,res){
+    res.redirect("/");
+})
 
 // start the server and make it listen on Port 3000 
 app.listen(3000, function(){
     console.log("server started and listenning on port 3000");
 })
-
-// API KEY for the mailchimp : 0bec30a1902d50532081aa2fe41ac587-us1
-// list of Audience          : 1adc9e2656
