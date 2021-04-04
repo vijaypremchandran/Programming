bd6494f93acbec2a9eaf2b9fa1f5ad0c5029ce0b
vijay chandran <vijayprem.chandran@gmail.com>
2021-04-03 23:50:58 -0400

    add ejs to Todo List Prj

--- Nodejs/TodoList_v1/app.js
+++ Nodejs/TodoList_v1/app.js
@@ -4,11 +4,53 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+// make our app use the ejs
+app.set('view engine', 'ejs');
 
 //get the default root / and send some response.
 
 app.get("/", function(req,res){
-    res.send("Hi there ");
+    // res.send("Hi there ");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    var dayName = "";
+
+    //check the day to find it is a weekday or weekend?
+    if(currentDay === 6 || currentDay === 0){ 
+        day = "Weekend";
+    }else{      
+        day = "Weekday";
+    };
+    
+    // use the switch validation to print the the day of the week.
+    switch(currentDay){
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+            console.log("Wrong day. check this.. " + currentDay)
+    };
+    // send the page using the ejs..KindofDay is the var in the list.ejs file inside the View fldr. 
+    res.render("list", {kindOfDay : day, kindDayName : dayName});
 });
 
 //start the server and listen at port 3000.
