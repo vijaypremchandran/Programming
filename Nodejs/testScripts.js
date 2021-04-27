581e7db53410157b628787ae17da9872b0dd75aa
vijay prem chandran <vijayprem.chandran@gmail.com>
2021-04-26 22:42:48 -0400

    adding challeges 20 completed

--- Nodejs/testScripts.js
+++ Nodejs/testScripts.js
@@ -1,15 +1,20 @@
 
-const blogPost = [{"Title" : "Post1",
-                  "blog"  : "Vijay"},
-                  {"Title" : "Post2",
-                  "blog"  : "Athul"},
-                  {"Title" : "Post3",
-                  "blog"  : "Eshaan"}]
+// const blogPost = [{"Title" : "Post1",
+//                   "blog"  : "Vijay"},
+//                   {"Title" : "Post2",
+//                   "blog"  : "Athul"},
+//                   {"Title" : "Post3",
+//                   "blog"  : "Eshaan"}]
 
-blogPost.forEach(post =>
-    console.log(post.Title)
-  );
+// blogPost.forEach(post =>
+//     console.log(post.Title)
+//   );
 
-  blogPost.forEach(function(post){
-    console.log(post.Title)
-  })
+//   blogPost.forEach(function(post){
+//     console.log(post.Title)
+//   })
+
+// try a sub string function .. 
+
+const str1 = "Hello World";
+console.log(str1.substring(0,4) + " ..");
