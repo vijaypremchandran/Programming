0f85085f484e4a6ab214df99d126991166cb231d
vijay prem chandran <vijayprem.chandran@gmail.com>
2021-07-05 10:39:19 -0400

    secrets app - leve1 authentication completed

---
+++ Nodejs/Secrets/app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+// Get all the required modules for running this app.
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+// Make the app use ejs and body parser and other resources in the public folder.
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//make the connection to Mongoose to the userDB.
+mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true, useUnifiedTopology: true  });
+
+// create a user schema.
+const userSchema = {
+    email : String,
+    password : String
+}
+
+// Create a model for the document.
+const User = new mongoose.model("User",userSchema);
+
+//render the pages thru GET 
+
+app.get("/",function(req, res){
+    res.render("home");
+})
+
+app.get("/login",function(req, res){
+    res.render("login");
+})
+
+app.get("/register",function(req, res){
+    res.render("register");
+})
+
+// catch the post request from the register route.. 
+app.post("/register", function(req,res){
+    const newUser = new User({
+        email   : req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+// catch the login route ..
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email : username}, function(err,foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                } 
+            } else {
+                console.log("invalid User");
+            }
+        }
+    });
+});
+
+// start the server at port 3000.
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
