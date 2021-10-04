16395a005cb9d97eb90c80c0360c1acf579084e0
vijaypremchandran <48194962+vijaypremchandran@users.noreply.github.com>
2021-10-03 23:39:17 -0400

    removed comments

--- Nodejs/NodeAws/nodeAwsCredentials.js
+++ Nodejs/NodeAws/nodeAwsCredentials.js
@@ -1,9 +1,5 @@
 var AWS = require('aws-sdk');
 
-// var myCredentials = new AWS.CognitoIdentityCredentials({IdentityPoolId:'AKIA543BN7XVSITP5FHC'});
-// var myConfig = new AWS.Config({
-//   credentials: myCredentials, region: 'us-west-2'
-// });
 
 AWS.config.getCredentials(function(err) {
     if (err) console.log(err.stack);
