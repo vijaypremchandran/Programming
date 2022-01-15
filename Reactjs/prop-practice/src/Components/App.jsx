53ccde8631a304efaadd6e8f11cd0a19ebe11692
vijay chandran <vijayprem.chandran@gmail.com>
2022-01-15 00:05:37 -0500

    added map function to the application

--- Reactjs/prop-practice/src/Components/App.jsx
+++ Reactjs/prop-practice/src/Components/App.jsx
@@ -2,30 +2,25 @@ import React from 'react';
 import Card from './Card';
 import contacts from '../contacts';
 
+//create a function to create card.
+
+function createCard(contacts){
+  return(
+    <Card
+      key={contacts.id} 
+      name={contacts.name}
+      imgURL={contacts.imgURL}
+      phone={contacts.phone}
+      email={contacts.email}
+    />
+  )
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-      name = {contacts[0].name} 
-      imgURL = {contacts[0].imgURL}
-      phone = {contacts[0].phone}
-      email = {contacts[0].email}
-      />
-
-<Card
-      name = {contacts[1].name} 
-      imgURL = {contacts[1].imgURL}
-      phone = {contacts[1].phone}
-      email = {contacts[1].email}
-      />
-
-<Card
-      name = {contacts[2].name} 
-      imgURL = {contacts[2].imgURL}
-      phone = {contacts[2].phone}
-      email = {contacts[2].email}
-      />
+      {contacts.map(createCard)};
     </div>
   );
 }
