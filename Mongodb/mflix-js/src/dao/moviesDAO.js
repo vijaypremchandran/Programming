b8560797ba4bd5ba3fc2bfd3eac2b913ec2736a2
vijay chandran <vijayprem.chandran@gmail.com>
2022-04-03 10:58:25 -0400

    completed Faceted search

--- Mongodb/mflix-js/src/dao/moviesDAO.js
+++ Mongodb/mflix-js/src/dao/moviesDAO.js
@@ -197,6 +197,9 @@ export default class MoviesDAO {
     const queryPipeline = [
       matchStage,
       sortStage,
+      skipStage,
+      limitStage,
+      facetStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
     ]
@@ -262,7 +265,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.skip(page * moviesPerPage).limit(moviesPerPage)
 
     try {
       const moviesList = await displayCursor.toArray()
